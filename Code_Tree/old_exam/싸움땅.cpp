db5313ae2c2fc7239054eed409fe879fb5c7da10
study_electronic <84219715+kimgeonoh@users.noreply.github.com>
2023-04-07 18:08:52 +0900

    Update 싸움땅.cpp

--- Code_Tree/old_exam/싸움땅.cpp
+++ Code_Tree/old_exam/싸움땅.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int n,m,k;
-int map[21][21]; // Gun, 0 빈칸, 그외 공격력
+vector<int> map[21][21]; // Gun, 0 빈칸, 그외 공격력
 int score[30];
 
 int dx[]={-1,0,1,0}; // 북동남서
@@ -34,7 +34,7 @@ void fight(int number){
     
     int nx = nowx + dx[nowd];
     int ny = nowy + dy[nowd];
-    if(nx<1||n<nx||ny<1||n<ny){ // 격자 밖인 경우
+    if(nx<1||n<nx||ny<1||n<ny){ // 격자 밖인 경우 방향 반대로
         nowd -= 2;
         if(nowd == -1) nowd = 3;
         if(nowd == -2) nowd = 2;
@@ -43,12 +43,30 @@ void fight(int number){
     }
 
     players[number] = PLAYER(nows,nowd,nx,ny); // 전진
+    
+    bool find = 0;
 
-    cout << players[number].x << players[number].y << players[number].d << '\n';
+    int index;
+    int index_x;
+    int index_y;
 
-    /*for(int i=0; i<m; i++){
-        if()
-    }*/
+    for(int i=0; i<m; i++){ // 전진한 곳에 플레이어가 존재하는가 탐색
+        if(i == number) continue; // 자기 자신 탐색은 제외
+        if(players[number].x==players[i].x && players[number].y==players[i].y){
+            // 플레이어가 존재한다면
+            find = 1; // find
+            index = i;
+            index_x = players[number].x;
+            index_y = players[number].y;
+        }
+    }
+
+    if(find){ // 플레이어가 존재한다면
+        ㅇ
+    }
+    else{ // 플레이어가 존재하지 않는다면
+        
+    }
     
 }
 
@@ -56,7 +74,8 @@ int main() {
     cin >> n >> m >> k;
     for(int i=1; i<=n; i++){
         for(int j=1; j<=n; j++){
-            cin >> map[i][j]; // Gun Map
+            int g; cin >> g;
+            map[i][j].push_back(g); // Gun Map
         }
     }
     for(int i=0; i<m; i++){
